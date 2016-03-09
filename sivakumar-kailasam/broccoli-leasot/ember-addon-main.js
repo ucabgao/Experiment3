d640c994f46842b03eb31c6c0a2ce0c28a69c3ee
simon.gaozheng <simon.gaozheng@gmail.com>
2016-03-09 06:50:16 +0000

    annotation

--- sivakumar-kailasam/broccoli-leasot/ember-addon-main.js
+++ sivakumar-kailasam/broccoli-leasot/ember-addon-main.js
@@ -1,44 +1,31 @@
+/// <reference path="node.d.ts" />
+"use strict";
 var path = require('path');
+//var path = require('path');
 var BroccoliBuilder = require('broccoli').Builder;
 var BroccoliLeasotFilter = require('./lib/index');
 var chalk = require('chalk');
 var forOwnFn = require('lodash/object').forOwn;
-
 module.exports = {
-
-	name: 'broccoli-leasot',
-
-	included: function(app) {
-
-		this._super.included.apply(this, arguments);
-
-		var pathToApp = path.resolve(app.project.root, app.trees.app);
-
-		var optionsForLeasot = {
-			enabled: true
-		};
-
-		var optionsThatCanBeOverridden = ['enabled', 'kinds', 'extensions', 'groupBy', 'console'];
-
-		if (typeof(app.options.markers) !== 'undefined') {
-			forOwnFn(app.options.markers, function(value, key) {
-				if (optionsThatCanBeOverridden.indexOf(key) !== -1) {
-					optionsForLeasot[key] = value;
-				} else {
-					console.log('\n' + chalk.bgRed(key + ' is an unknown option for broccoli-leasot'));
-				}
-			});
-		}
-
-		console.log('\n');
-
-		new BroccoliBuilder(
-			new BroccoliLeasotFilter(
-				pathToApp,
-				optionsForLeasot
-			)
-		).build();
-
-	}
-
+    name: 'broccoli-leasot',
+    included: function (app) {
+        this._super.included.apply(this, arguments);
+        var pathToApp = path.resolve(app.project.root, app.trees.app);
+        var optionsForLeasot = {
+            enabled: true
+        };
+        var optionsThatCanBeOverridden = ['enabled', 'kinds', 'extensions', 'groupBy', 'console'];
+        if (typeof (app.options.markers) !== 'undefined') {
+            forOwnFn(app.options.markers, function (value, key) {
+                if (optionsThatCanBeOverridden.indexOf(key) !== -1) {
+                    optionsForLeasot[key] = value;
+                }
+                else {
+                    console.log('\n' + chalk.bgRed(key + ' is an unknown option for broccoli-leasot'));
+                }
+            });
+        }
+        console.log('\n');
+        new BroccoliBuilder(new BroccoliLeasotFilter(pathToApp, optionsForLeasot)).build();
+    }
 };
