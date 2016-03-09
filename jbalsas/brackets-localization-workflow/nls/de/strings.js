3a1cb335b86a76f7cccd69301c1f09f478630189
simon.gaozheng <simon.gaozheng@gmail.com>
2016-03-09 02:53:19 +0000

    annotation

--- jbalsas/brackets-localization-workflow/nls/de/strings.js
+++ jbalsas/brackets-localization-workflow/nls/de/strings.js
@@ -1,15 +1,13 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
-
 define({
-    "SHOW_STATUS_CMD"           : "Zeige Lokalisierungsstatus",
-    "STRING_HEADER"             : "Text",
-    "STATUS_HEADER"             : "Status",
-    "MISSING_STRING_DESC"       : "Diese Zeichenkette fehlt in der Sprachdatei",
-    "UNUSED_STRING_DESC"        : "Diese Zeichenkette wird in der Hauptdatei nicht genutzt",
-    "UNTRANSLATED_STRING_DESC"  : "Die beiden Zeichenketten in Haupt- und Sprachdatei scheinen gleich zu sein"
-    
-    "IGNORE"        : "Ignorieren",
-    "STOP_IGNORING" : "Nicht mehr ignorieren",
-    "SHOW_IGNORED"  : "Ignorierte Texte anzeigen"
+    "SHOW_STATUS_CMD": "Zeige Lokalisierungsstatus",
+    "STRING_HEADER": "Text",
+    "STATUS_HEADER": "Status",
+    "MISSING_STRING_DESC": "Diese Zeichenkette fehlt in der Sprachdatei",
+    "UNUSED_STRING_DESC": "Diese Zeichenkette wird in der Hauptdatei nicht genutzt",
+    "UNTRANSLATED_STRING_DESC": "Die beiden Zeichenketten in Haupt- und Sprachdatei scheinen gleich zu sein",
+    "IGNORE": "Ignorieren",
+    "STOP_IGNORING": "Nicht mehr ignorieren",
+    "SHOW_IGNORED": "Ignorierte Texte anzeigen"
 });
