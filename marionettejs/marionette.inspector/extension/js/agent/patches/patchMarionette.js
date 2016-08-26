d2e4b6d75e399c8a87d93a970f3bff1837fa29ab
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-26 06:38:40 +0100

    annotation

--- marionettejs/marionette.inspector/extension/js/agent/patches/patchMarionette.js
+++ marionettejs/marionette.inspector/extension/js/agent/patches/patchMarionette.js
@@ -2,7 +2,7 @@
 
 ;
 
-class t { 
+interface t { 
 ItemView:any;
 CollectionView:any;
 CompositeView:any;
@@ -14,6 +14,7 @@ Controller:any;
 VERSION:any;
 Region:any;
 Object:any;
+Layout:any;
 }
 
 (function(Agent){
